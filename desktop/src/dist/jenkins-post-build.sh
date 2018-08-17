eae2da45e035c0bdb44e8e7a3a0f6985a0fe4b41
Artur Bassov <artiku0226@gmail.com>
2018-08-17 14:01:34 +0300

    Add docker --version to the script

--- desktop/src/dist/jenkins-post-build.sh
+++ desktop/src/dist/jenkins-post-build.sh
@@ -2,6 +2,7 @@ pwd
 ls
 cp desktop/build/libs/desktop.jar desktop/src/dist/
 ls
+docker --version
 docker infodocker build -t etherdale/test-demo:${BUILD_NUMBER} .
 docker tag etherdale/test-demo:${BUILD_NUMBER} etherdale/test-demo:latest
 docker images
